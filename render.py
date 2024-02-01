e61c5112a8e330ab934dab59e228330d00bd8baa
Kevin W <36448536+kevinw7789@users.noreply.github.com>
2024-02-01 16:50:32 -0500

    py plot

--- render.py
+++ render.py
@@ -1,5 +1,3 @@
-import time
-import requests
 import csv
 import folium
 
@@ -12,10 +10,23 @@ import matplotlib.pyplot as plt
 center = [40.641766, -73.780968]
 mymap = folium.Map(location=center, zoom_start=5)
 
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+lat = []
+lon = []
+alt = []
+
 with open('data.csv', 'r') as file:
     reader = csv.reader(file)
 
     for row in reader:
+        lat.append(row[0])
+        lon.append(row[1])
+        alt.append(row[2])
         folium.Marker(location=[row[0], row[1]]).add_to(mymap)
 
+ax.grid()
+ax.plot(lat,lon,alt, marker='o', markersize=5)
+
 mymap.save("map.html")
+plt.show()
